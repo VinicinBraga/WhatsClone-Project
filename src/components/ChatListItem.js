f382245c9109c188be32702769b1c6d2fbcefbc5
VinicinBraga <vinicius@bragamatta.com.br>
2021-08-31 18:28:48 -0300

    Colocando horario no chat

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ChatListItem.css";
 
 export default function ChatListItem({ onClick, active, data }) {
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    if (data.lastMessageDate > 0) {
+      let d = new Date(data.lastMessageDate.seconds * 1000);
+      console.log(d);
+      let hours = d.getHours();
+      let minutes = d.getMinutes();
+      hours = hours < 10 ? "0" + minutes : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      setTime(`${hours}:${minutes}`);
+    }
+  }, [data]);
+
   return (
     <div className={`chatListItem ${active ? "active" : ""}`} onClick={onClick}>
       <img className="chatListItem--avatar" src={data.image} alt="" />
       <div className="chatListItem--lines">
         <div className="chatListItem--line">
           <div className="chatlist--name">{data.title}</div>
-          <div className="chatlist--date">19:00</div>
+          <div className="chatlist--date">{time}</div>
         </div>
         <div className="chatListItem--line">
           <div className="chatlist--lastMsg">
