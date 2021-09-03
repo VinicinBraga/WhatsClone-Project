1c8866df75b5cb27673e84891ec00004b5ad1e73
VinicinBraga <vinicius@bragamatta.com.br>
2021-09-03 14:55:35 -0300

    Finalizando as funcionalidades do chat

--- src/Api.js
+++ src/Api.js
@@ -71,7 +71,22 @@ export default {
         if (doc.exists) {
           let data = doc.data();
           if (data.chats) {
-            setChatList(data.chats);
+            let chats = [...data.chats];
+
+            chats.sort((a, b) => {
+              if (a.lastMessageDate === undefined) {
+                return -1;
+              }
+              if (b.lastMessageDate === undefined) {
+                return -1;
+              }
+              if (a.lastMessageDate.seconds < b.lastMessageDate.seconds) {
+                return 1;
+              } else {
+                return -1;
+              }
+            });
+            setChatList(chats);
           }
         }
       });
